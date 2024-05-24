070f16f8078a32d17217c2b4102ce7067f115572
JayBro999Git <147154295+JayBro999Git@users.noreply.github.com>
2024-05-24 15:18:09 +1000

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -58,21 +58,21 @@ export function Header() {
       <div className="flex items-center justify-end space-x-2">
         <a
           target="_blank"
-          href="https://github.com/vercel/nextjs-ai-chatbot/"
+          href="https://github.com/JayBro999Git"
           rel="noopener noreferrer"
           className={cn(buttonVariants({ variant: 'outline' }))}
         >
           <IconGitHub />
-          <span className="hidden ml-2 md:flex">GitHub</span>
+          <span className="hidden ml-2 md:flex">Made by A.B.</span>
         </a>
         <a
-          href="https://vercel.com/templates/Next.js/nextjs-ai-chatbot"
+          href="https://example.org/"
           target="_blank"
           className={cn(buttonVariants())}
         >
           <IconVercel className="mr-2" />
-          <span className="hidden sm:block">Deploy to Vercel</span>
-          <span className="sm:hidden">Deploy</span>
+          <span className="hidden sm:block">support the boys</span>
+          <span className="sm:hidden">support</span>
         </a>
       </div>
     </header>
