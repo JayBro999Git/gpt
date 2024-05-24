10084e70cda5ec3d1a252365af21f0c880a08c9c
JayBro999Git <147154295+JayBro999Git@users.noreply.github.com>
2024-05-24 14:31:15 +1000

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -1,23 +1,23 @@
-import * as React from 'react';
-import Link from 'next/link';
-import { cn } from '@/lib/utils';
-import { auth } from '@/auth';
-import { Button, buttonVariants } from '@/components/ui/button';
+import * as React from 'react'
+import Link from 'next/link'
+
+import { cn } from '@/lib/utils'
+import { auth } from '@/auth'
+import { Button, buttonVariants } from '@/components/ui/button'
 import {
   IconGitHub,
   IconNextChat,
   IconSeparator,
   IconVercel
-} from '@/components/ui/icons';
-import { UserMenu } from '@/components/user-menu';
-import { SidebarMobile } from './sidebar-mobile';
-import { SidebarToggle } from './sidebar-toggle';
-import { ChatHistory } from './chat-history';
-import { Session } from '@/lib/types';
+} from '@/components/ui/icons'
+import { UserMenu } from '@/components/user-menu'
+import { SidebarMobile } from './sidebar-mobile'
+import { SidebarToggle } from './sidebar-toggle'
+import { ChatHistory } from './chat-history'
+import { Session } from '@/lib/types'
 
 async function UserOrLogin() {
-  const session = (await auth()) as Session;
-  
+  const session = (await auth()) as Session
   return (
     <>
       {session?.user ? (
@@ -44,7 +44,7 @@ async function UserOrLogin() {
         )}
       </div>
     </>
-  );
+  )
 }
 
 export function Header() {
@@ -76,7 +76,5 @@ export function Header() {
         </a>
       </div>
     </header>
-  );
+  )
 }
-
-export default Header;
